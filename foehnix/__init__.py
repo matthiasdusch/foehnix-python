8e578fe0502fbec1213175e40ad1d195e1b5956f
Matthias Dusch <matthias.dusch@uibk.ac.at>
2019-02-22 01:46:51 +0100

    hovmoeller advances

--- foehnix/__init__.py
+++ foehnix/__init__.py
@@ -1,9 +1,9 @@
 # API to access the main class as foehnix.Foehnix(...) after import foehnix
-from foehnix.foehnix import Foehnix
-from foehnix.foehnix_filter import foehnix_filter, filter_summary
-from foehnix.families import Family, GaussianFamily
-from foehnix.iwls_logit import iwls_logit
-from foehnix.demodata import get_demodata
+from .foehnix import Foehnix
+from .foehnix_filter import foehnix_filter, filter_summary
+from .families import Family, GaussianFamily
+from .iwls_logit import iwls_logit
+from .demodata import get_demodata
 
 # __version__
 from foehnix import version
